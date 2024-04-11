f55e84c34ee995d09d4ef23f112c118b7d612713
Hauke Licht <hlicht@eu-login-29.euler.ethz.ch>
2024-04-11 16:16:10 +0200

    updated info ranking bash script

--- code/sampling/rank_manifesto_sentences.bash
+++ code/sampling/rank_manifesto_sentences.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --time=01:00:00
-#SBATCH --gpus=a100-pcie-40gb:4
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:40G
 #SBATCH --mem-per-cpu=32G
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=hauke.licht@wiso.uni-koeln.de
-#SBATCH --job-name=manifest_sentences_informativeness_ranking_test
+#SBATCH --job-name=manifest_sentences_informativeness_ranking
 #SBATCH --output=%x.log
 #SBATCH --error=%x.err
 
@@ -12,12 +13,13 @@ module load gcc/8.2.0 eth_proxy python_gpu/3.11.2
 source ./../../venv/bin/activate
 
 DATAPATH='../../data/manifestos'
+DATAFILE='all_manifesto_sentences'
 
 python informativeness_ranking.py \
-    --input_file "$DATAPATH/test_sentences.tsv" \
+    --input_file "$DATAPATH/${DATAFILE}.tsv" \
     --text_col 'text' \
     --id_col 'sentence_id' \
-    --output_file "$DATAPATH/test_sentences_informativeness_ranking.tsv" --overwrite_output_file \
+    --output_file "$DATAPATH/${DATAFILE}_informativeness_ranking.tsv" --overwrite_output_file \
     --embedding_model 'intfloat/multilingual-e5-large-instruct' --embedding_batch_size 256 \
     --device 'cuda:0' \
     --seed 1234 \
