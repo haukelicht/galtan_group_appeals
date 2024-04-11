2e69584adb31829e2fd1479d1ef2f6960b7e2d72
Hauke Licht <hauke.licht@t-online.de>
2024-04-11 11:22:51 +0200

    prepared info ranking script for EULER

--- code/sampling/rank_manifesto_sentences.bash
+++ code/sampling/rank_manifesto_sentences.bash
@@ -1,8 +1,23 @@
+#SBATCH --time=01:00:00
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:40G
+#SBATCH --mem-per-cpu=32G
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=hauke.licht@wiso.uni-koeln.de
+#SBATCH --job-name=manifest_sentences_informativeness_ranking_test
+#SBATCH --output=%x.log
+#SBATCH --error=%x.err
+
+module load gcc/8.2.0 eth_proxy python_gpu/3.11.2
+source ./../../venv/bin/activate
+
+DATAPATH='../../data/manifestos'
+
 pyhton3 informativeness_ranking.py \
-    --input_file 'all_manifesto_sentences.tsv' \
+    --input_file "$DATAPATH/'test_sentences.tsv" \
     --text_col 'text' \
     --id_col 'sentence_id' \
-    --output_file 'all_manifesto_sentences_informativeness_ranking.tsv' --overwrite_output_file \
+    --output_file "$DATAPATH/test_sentences_informativeness_ranking.tsv" --overwrite_output_file \
     --embedding_model 'intfloat/multilingual-e5-large-instruct' \
     --device 'cuda:0' \
     --seed 1234 \
