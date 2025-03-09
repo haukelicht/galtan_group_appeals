58b1024a1e67ea8a356a1bccc83b19dec0f5a38c
Hauke Licht <hauke.licht@t-online.de>
2025-03-09 19:39:19 +0100

    added script for running ICL in background

---
+++ code/mention-classification/_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CONDAENV=$(conda info | grep "active environment" | awk '{print $NF}')
+
+if [[ "$CONDAENV" != "parl_speech_actors" ]]; then
+    echo "Error: Active environment is not 'parl_speech_actors'."
+    exit 1
+fi
+
+# run in background
+nohup ./run_workflow_ollama.sh > "logs/workflow_$(date '+%Y-%m-%dT%H:%M:%S').log" 2>&1 &
