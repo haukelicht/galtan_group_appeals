39aab671de4734fe1152d4b7b7b9867c09d01d98
Hauke Licht <hauke.licht@uibk.ac.at>
2025-11-30 10:20:34 +0100

    added code for and results from  mention attribute classification error analysis step

---
+++ code/mention-classification/round5/_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONDAENV=$(conda info | grep "active environment" | awk '{print $NF}')
+
+if [[ "$CONDAENV" != "galtan_group_appeals" ]]; then
+    echo "Error: Active environment is not 'galtan_group_appeals'."
+    exit 1
+fi
+
+mkdir -p logs
+
+nohup ./06-mention-attribute-dimension-classification_setfit_error_detection.sh > "logs/mention_attribute_dimension_classification.log" 2>&1 &
+nohup ./06-mention-economic-attributes-classification_setfit_error_detection.sh > "logs/mention_economic_attributes_classification.log" 2>&1 &
+nohup ./06-mention-noneconomic-attributes-classification_setfit_error_detection.sh > "logs/mention_noneconomic_attributes_classification.log" 2>&1 &
---
+++ data/annotations/group_mention_categorization/attribute-misclassification-review/download.sh
@@ -0,0 +1,2 @@
+#!bin/bash
+curl -L 'https://docs.google.com/spreadsheets/d/1LMYul5tgsLstnTNAndKsknPmtFuA2uc-/export?exportFormat=excel' -o reviewed.xlsx
